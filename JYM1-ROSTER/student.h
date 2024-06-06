0b2dab8b07461735b2104bd60127d3cb2f88a04a
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-06-06 10:57:32 -0700

    Finish creating Class description for student

--- JYM1-ROSTER/student.h
+++ JYM1-ROSTER/student.h
@@ -1,9 +1,45 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+#include <string>
+#include <degree.h>
+
 class Student
 {
-    
-}
+private:
+    std::string student_id;
+    std::string first_name;
+    std::string last_name;
+    std::string email;
+    int age;
+    int num_days[3];
+    DegreeProgram degree_program;
+
+public:
+    // Constructor all input params
+    Student(std::string student_id, std::string first_name, std::string last_name,
+            std::string email, int age, int num_days[], DegreeProgram degree_program);
+
+    // Accessor/Getter methods
+    std::string get_student_id();
+    std::string get_first_name();
+    std::string get_last_name();
+    std::string get_email();
+    int get_age();
+    int get_num_days();
+    DegreeProgram get_degree_program();
+
+    // Mutator/Setter methods
+    std::string set_student_id();
+    std::string set_first_name();
+    std::string set_last_name();
+    std::string set_email();
+    int set_age();
+    int set_num_days();
+    DegreeProgram set_degree_program();
+
+    // Print method
+    void print();
+};
 
 #endif
